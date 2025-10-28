a7e41623efed50bdff0bb835a7ccb0c7f08a57d3
Lara Luiza <laraluizaqueiroz@gmail.com>
2025-10-28 12:16:26 -0300

    Create atividadeRecuperação.php

---
+++ Controller/atividadeRecuperação.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'Model/User.php';
+require_once 'Controller/UserController.php';
+
+use Model\User;
+use Controller\UserController;
+
+// Verifica se o formulario foi enviado
+if 
+
+?>
